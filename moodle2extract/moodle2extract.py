e061d24a43598b256f4abee4cf26f0b886269362
Cristian Alvarado <ocdude@bluewavedigital.net>
2015-11-25 15:35:04 -0800

    Cleaned up code, added description

--- moodle2extract/moodle2extract.py
+++ moodle2extract/moodle2extract.py
@@ -4,9 +4,15 @@ import argparse
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input",metavar="MOODLE BACKUP FILE",help="A .mbz moodle backup file.")
-    parser.add_argument("-o",metavar="OUTPUT DIR",help="Output directory. If omitted, files will be extracted to a folder in the current directory")
+    parser = argparse.ArgumentParser(description='''A utility to extract files
+        from a .mbz Moodle backup file''')
+    parser.add_argument("input",
+        metavar="MOODLE BACKUP FILE",
+        help="A .mbz moodle backup file")
+    parser.add_argument("-o",
+        metavar="OUTPUT DIR",
+        help='''Set output directory. If omitted, files will be extracted to a
+            folder in the current directory''')
 
     args = parser.parse_args()
     m = mbz.MBZ(args.o)
