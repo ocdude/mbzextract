ad5b1cc704948211fc398632c6dd7d6a7e52a083
Cristian Alvarado <ocdude@bluewavedigital.net>
2015-12-01 09:54:22 -0800

    PEP8

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from moodle2extract.mbz import __version__
 from setuptools import setup, find_packages
 
 packages = find_packages()
-package_dir = {'':'plugins'}
+package_dir = {'': 'plugins'}
 
 setup(name='moodle2extract',
-	version=__version__,
-	description='Utility to extract data from a Moodle2 backup file',
-	author='Cristian Alvarado',
-	author_email='ocdude@bluewavedigital.net',
-	packages=packages,
-	install_requires=[
-		'jinja2>=2.7',
-		],
-	scripts=['bin/moodle2extract']
-)
+      version=__version__,
+      description='Utility to extract data from a Moodle2 backup file',
+      author='Cristian Alvarado',
+      author_email='ocdude@bluewavedigital.net',
+      packages=packages,
+      install_requires=[
+          'jinja2>=2.7',
+      ],
+      scripts=['bin/moodle2extract']
+      )
