5359df452d240e985468ba9690b1451226641360
Cristian Alvarado <ocdude@bluewavedigital.net>
2015-12-01 16:49:52 -0800

    Fixed check to see if backup had student data

--- moodle2extract/plugins/assignment/assignment.py
+++ moodle2extract/plugins/assignment/assignment.py
@@ -24,6 +24,8 @@ class moodle_module:
             query = 'CREATE TABLE IF NOT EXISTS assignment_submissions (submissionid int PRIMARY KEY,activityid int,userid int,timecreated int,timemodified int,data text,grade real,comment text,teacher int,timemarked int)'
             self.db_cursor.execute(query)
             self.student_data = kwargs['student_data']
+        else:
+            self.student_data = False
 
         # commit the changes
         self.db.commit()
@@ -79,97 +81,98 @@ class moodle_module:
         if os.path.exists(path) == False:
             os.makedirs(path)
         os.chdir(path)
-        self.db_cursor.execute(
-            'SELECT submissionid,userid,timemodified,data,grade,comment,teacher,timemarked FROM assignment_submissions WHERE activityid=? ORDER BY timemodified DESC', (self.current_id,))
-        sub_results = self.db_cursor.fetchall()
-        submissions = []
-        if results[5] == 'online' or results[5] == 'offline':
-            # extract online text
-            for sub in sub_results:
-                # grab name of student from db
-                self.db_cursor.execute(
-                    'SELECT firstname,lastname FROM users WHERE userid=?', (sub[1],))
-                user = self.db_cursor.fetchone()
-                username = user[0] + " " + user[1]
+        if self.student_data == True:
+            self.db_cursor.execute(
+                'SELECT submissionid,userid,timemodified,data,grade,comment,teacher,timemarked FROM assignment_submissions WHERE activityid=? ORDER BY timemodified DESC', (self.current_id,))
+            sub_results = self.db_cursor.fetchall()
+            submissions = []
+            if results[5] == 'online' or results[5] == 'offline':
+                # extract online text
+                for sub in sub_results:
+                    # grab name of student from db
+                    self.db_cursor.execute(
+                        'SELECT firstname,lastname FROM users WHERE userid=?', (sub[1],))
+                    user = self.db_cursor.fetchone()
+                    username = user[0] + " " + user[1]
 
-                # grab name of teacher from db
-                self.db_cursor.execute(
-                    'SELECT firstname,lastname FROM users WHERE userid=?', (sub[6],))
-                teacher = self.db_cursor.fetchone()
-                if teacher is not None:
-                    grader = teacher[0] + " " + teacher[1]
-                else:
-                    grader = ""
+                    # grab name of teacher from db
+                    self.db_cursor.execute(
+                        'SELECT firstname,lastname FROM users WHERE userid=?', (sub[6],))
+                    teacher = self.db_cursor.fetchone()
+                    if teacher is not None:
+                        grader = teacher[0] + " " + teacher[1]
+                    else:
+                        grader = ""
 
-                # construct submission
-                submissions.append({'id': sub[0],
-                                    'user': username,
-                                    'timemodified': datetime.fromtimestamp(sub[2]),
-                                    'data': sub[3],
-                                    'grade': sub[4],
-                                    'comment': sub[5],
-                                    'teacher': grader,
-                                    'timemarked': sub[7]})
-            template = self.env.get_template('online_text.html')
-            output = template.render(name=results[3],
-                                     intro=results[4],
-                                     student_data=self.student_data,
-                                     submissions=submissions)
-            os.chdir(path)
-            with open('assignment.html', 'w+') as f:
+                    # construct submission
+                    submissions.append({'id': sub[0],
+                                        'user': username,
+                                        'timemodified': datetime.fromtimestamp(sub[2]),
+                                        'data': sub[3],
+                                        'grade': sub[4],
+                                        'comment': sub[5],
+                                        'teacher': grader,
+                                        'timemarked': sub[7]})
+                template = self.env.get_template('online_text.html')
+                output = template.render(name=results[3],
+                                         intro=results[4],
+                                         student_data=self.student_data,
+                                         submissions=submissions)
+                os.chdir(path)
+                with open('assignment.html', 'w+') as f:
+                    f.write(output)
+                    f.close()
+            elif results[5] == 'upload' or results[5] == 'uploadsingle':
+
+                for sub in sub_results:
+                    # grab name of student from db
+                    self.db_cursor.execute(
+                        'SELECT firstname,lastname FROM users WHERE userid=?', (sub[1],))
+                    user = self.db_cursor.fetchone()
+                    username = user[0] + " " + user[1]
+
+                    # grab name of teacher from db
+                    self.db_cursor.execute(
+                        'SELECT firstname,lastname FROM users WHERE userid=?', (sub[6],))
+                    teacher = self.db_cursor.fetchone()
+                    if teacher is not None:
+                        grader = teacher[0] + " " + teacher[1]
+                    else:
+                        grader = ""
+
+                    # construct submission
+                    submissions.append({'id': sub[0],
+                                        'user': username,
+                                        'timemodified': datetime.fromtimestamp(sub[2]),
+                                        'grade': sub[4],
+                                        'comment': sub[5],
+                                        'teacher': grader,
+                                        'timemarked': sub[7]})
+
+                    # grab all files submitted by this student
+                    self.db_cursor.execute(
+                        'SELECT contenthash,contextid,filename,userid FROM files WHERE userid=? AND contextid=?', (sub[1], results[2]))
+                    files = self.db_cursor.fetchall()
+                    submitted_files = []
+                    if files is not None:
+                        for f in files:
+                            os.chdir(self.temp_dir)
+                            if not os.path.exists(os.path.join(path, username)):
+                                os.makedirs(os.path.join(path, username))
+                            self.backup.extract_file(
+                                f[0], os.path.join(path, username, f[2]))
+                            # construct file list
+                            submitted_files.append(
+                                {'url': 'file://' + os.path.join(path, username, f[2]), 'filename': f[2]})
+
+                # write the output assignment.html
+                template = self.env.get_template('upload.html')
+                output = template.render(name=results[3],
+                                         intro=results[4],
+                                         student_data=self.student_data,
+                                         submissions=submissions,
+                                         files=submitted_files)
+                os.chdir(path)
+                f = open('assignment.html', 'w+')
                 f.write(output)
                 f.close()
-        elif results[5] == 'upload' or results[5] == 'uploadsingle':
-
-            for sub in sub_results:
-                # grab name of student from db
-                self.db_cursor.execute(
-                    'SELECT firstname,lastname FROM users WHERE userid=?', (sub[1],))
-                user = self.db_cursor.fetchone()
-                username = user[0] + " " + user[1]
-
-                # grab name of teacher from db
-                self.db_cursor.execute(
-                    'SELECT firstname,lastname FROM users WHERE userid=?', (sub[6],))
-                teacher = self.db_cursor.fetchone()
-                if teacher is not None:
-                    grader = teacher[0] + " " + teacher[1]
-                else:
-                    grader = ""
-
-                # construct submission
-                submissions.append({'id': sub[0],
-                                    'user': username,
-                                    'timemodified': datetime.fromtimestamp(sub[2]),
-                                    'grade': sub[4],
-                                    'comment': sub[5],
-                                    'teacher': grader,
-                                    'timemarked': sub[7]})
-
-                # grab all files submitted by this student
-                self.db_cursor.execute(
-                    'SELECT contenthash,contextid,filename,userid FROM files WHERE userid=? AND contextid=?', (sub[1], results[2]))
-                files = self.db_cursor.fetchall()
-                submitted_files = []
-                if files is not None:
-                    for f in files:
-                        os.chdir(self.temp_dir)
-                        if not os.path.exists(os.path.join(path, username)):
-                            os.makedirs(os.path.join(path, username))
-                        self.backup.extract_file(
-                            f[0], os.path.join(path, username, f[2]))
-                        # construct file list
-                        submitted_files.append(
-                            {'url': 'file://' + os.path.join(path, username, f[2]), 'filename': f[2]})
-
-            # write the output assignment.html
-            template = self.env.get_template('upload.html')
-            output = template.render(name=results[3],
-                                     intro=results[4],
-                                     student_data=self.student_data,
-                                     submissions=submissions,
-                                     files=submitted_files)
-            os.chdir(path)
-            f = open('assignment.html', 'w+')
-            f.write(output)
-            f.close()
